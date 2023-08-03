ac0c861dde8932c287e3bb8a6866c42cdd178b26
Mas <139600216+notmasmas@users.noreply.github.com>
2023-08-03 17:12:09 -0300

    Create Entity.py

---
+++ Entity.py
@@ -0,0 +1,17 @@
+import pygame
+class Entity:
+
+    def __int__(self):
+        self.x = 0
+        self.y = 0
+
+    def spawn(self, x, y):
+        self.x = x
+        self.y = y
+
+    def update(self):
+        pass
+
+    def draw(self, screen):
+
+        pass
