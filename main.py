f747a10ecf932fef9c5221644824ef846f5ef5a1
notneat <aaron.hurter@gmail.com>
2023-08-03 22:24:49 -0300

    melhorei :D

--- main.py
+++ main.py
@@ -1,36 +1,35 @@
 import pygame
 from sys import exit
-import random
 
-from Entity import Entity
+from pygame import surface
+
+# import random
+# from entity import *
 
 pygame.init()
 
 screen = pygame.display.set_mode((640, 480))
+pygame.display.set_caption("Kiwi :D")
 clock = pygame.time.Clock()
 
 screen_width = screen.get_width() / 2
 screen_height = screen.get_height() / 2
 
-entity = Entity()
-entity.spawn(screen_width, screen_height)
-entity_out_of_screen = False
-fruit_spawn_x = random.randrange(25, 615)
-fruit_spawn_y = 0
+kiwi_surf = pygame.image.load("graphics/kiwi.png")
+kiwi_rect = kiwi_surf.get_rect(midbottom=(screen_width, 440))
 
-player_pos = pygame.Vector2(screen_width, 0)
-player_size = 25
-
-gravity = 0
+ground_height = 40
+ground_color = (34,139,34)
+ground = pygame.Rect(0, screen_height * 2 - ground_height, screen_width * 2, ground_height)
 
 
 def movement():
-    player_pos.x = pygame.mouse.get_pos()[0]
+    kiwi_rect.x = pygame.mouse.get_pos()[0]
     keys = pygame.key.get_pressed()
     if keys[pygame.K_a]:
-        player_pos.x -= 5
+        kiwi_rect.x -= 5
     if keys[pygame.K_d]:
-        player_pos.x += 5
+        kiwi_rect.x += 5
 
 
 while True:
@@ -39,15 +38,10 @@ while True:
             pygame.quit()
             exit()
 
-        gravity += 1
-        player_pos.y += gravity
-
-        screen.fill("white")
-
-        movement()
-        entity.update()
-
-    pygame.draw.circle(screen, "black", player_pos, player_size)
+    screen.fill("white")
+    pygame.draw.rect(screen, ground_color, ground)
+    screen.blit(kiwi_surf, (kiwi_rect.x, kiwi_rect.y))
+    movement()
 
     pygame.display.update()
     clock.tick(60)
